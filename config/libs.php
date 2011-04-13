892b8c113675eede29ea7f57c4efedc9fe6c2e88
Dean Sofer <proloser@hotmail.com>
2011-04-13 04:43:40 -0700

    Cleaned up helpers for sake of sanity:
    - Removed unused methods from plate/htmlplus
    - Rewrote 1 standard approach for iecc
    - Rewrote open/start, renamed html(), to only handle the html tags
    - JsLib() renamed to lib() to prevent casing confusion
    - all lib() is now case independant
    - Rewrote almost all methods to be more optimized and readable
    - Formatted indentation and docblocks
    - Reduced poorly scoped methods:
      - open() included the doctype and charset which are as easily inserted manually
      - cdns/fallbacks/paths to libs were cluttered and can easily be broken. Fallback only works for jquery. Paths may easily be broken
      - removed html5 doctype. longer to use php than raw html (see lighthouseapp cake2.0)
      - removed HtmlPlus->meta(), unnecessary abstraction and easier to do manually
      - removed some Plate methods because they create an unnecessary abstraction

---
+++ config/libs.php
@@ -0,0 +1,22 @@
+<?php
+$config['BakingPlate']['Libs']['modernizr'] = array(
+	'cdn' => 'libs/modernizr-1.7.min',
+);
+$config['BakingPlate']['Libs']['firebug'] = array(
+	'cdn' => 'https://getfirebug.com/firebug-lite.js'
+);
+$config['BakingPlate']['Libs']['css3-mediaqueries'] = array(
+	'cdn' => 'libs/css3-mediaqueries',
+);
+$config['BakingPlate']['Libs']['jquery'] = array(
+	'cdn' => '//ajax.googleapis.com/ajax/libs/jquery/:version/jquery.js',
+	'cdnu' => '//ajax.googleapis.com/ajax/libs/jquery/:version/jquery.min.js',
+	'version' => '1.5.2',
+);
+$config['BakingPlate']['Libs']['swfobject'] = array(
+	'cdn' => '//ajax.googleapis.com/ajax/libs/swfobject/:version/swfobject.js',
+	'cdnu' => '',
+);
+$config['BakingPlate']['Libs']['pngfix'] = array(
+	'cdn' => 'libs/dd_belatedpng',
+);
