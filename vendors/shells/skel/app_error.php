13a1e4db3fb9114cdc1c111ca7c76a717d3d82e5
sam <sam.sherlock@gmail.com>
2011-04-22 08:18:45 +0100

    re add doc link to app error

--- vendors/shells/skel/app_error.php
+++ vendors/shells/skel/app_error.php
@@ -2,6 +2,10 @@
 class AppError extends ErrorHandler {
 	
 	function _outputMessage($template) {
+		/*
+		 * Use an error layout 
+		 * @link http://nuts-and-bolts-of-cakephp.com/2009/04/30/give-all-of-your-error-messages-a-different-layout/
+		 */
 		$this->controller->layout = 'error';
 		return parent::_outputMessage($template);
 	}
