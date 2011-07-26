8d44313fc513c16616009c59203526149c806b5b
Dean Sofer <proloser@hotmail.com>
2011-07-26 01:27:20 -0700

    Added nifty placeholder automagic to FormPlusHelper

--- views/helpers/form_plus.php
+++ views/helpers/form_plus.php
@@ -184,7 +184,7 @@ class FormPlusHelper extends FormHelper {
 				unset($options['options']);
 			}
 		}
-
+		
 		if ($label !== false) {
 			$label = $this->_inputLabel($fieldName, $label, $options);
 		}
@@ -282,5 +282,76 @@ class FormPlusHelper extends FormHelper {
 			$output = $this->Html->tag($tag, $output, $divOptions);
 		}
 		return $output;
+	}
+	
+/**
+ * Creates a text input widget.
+ *
+ * @param string $fieldName Name of a field, in the form "Modelname.fieldname"
+ * @param array $options Array of HTML attributes.
+ * @return string A generated HTML text input element
+ * @access public
+ * @link http://book.cakephp.org/view/1432/text
+ */
+	function text($fieldName, $options = array()) {
+		$options = $this->_addPlaceholder($fieldName, $options);
+		return parent::text($fieldName, $options);
+	}
+	
+/**
+ * Creates a textarea widget.
+ *
+ * ### Options:
+ *
+ * - `escape` - Whether or not the contents of the textarea should be escaped. Defaults to true.
+ *
+ * @param string $fieldName Name of a field, in the form "Modelname.fieldname"
+ * @param array $options Array of HTML attributes, and special options above.
+ * @return string A generated HTML text input element
+ * @access public
+ * @link http://book.cakephp.org/view/1433/textarea
+ */
+	function textarea($fieldName, $options = array()) {
+		$options = $this->_addPlaceholder($fieldName, $options);
+		return parent::textarea($fieldName, $options);
+	}
+	
+/**
+ * Formats a string into a human-readable format
+ *
+ * @param string $text 
+ * @return string A formatted version of $text
+ */
+	function _labelText($text) {
+		if (strpos($text, '.') !== false) {
+			$text = array_pop(explode('.', $text));
+		} else {
+			$text = $text;
+		}
+		if (substr($text, -3) == '_id') {
+			$text = substr($text, 0, strlen($text) - 3);
+		}
+		$text = __(Inflector::humanize(Inflector::underscore($text)), true);
+		return $text;
+	}
+
+/**
+ * Supplements an $options array with placeholder attributes
+ *
+ * @param string $fieldName 
+ * @param string $options 
+ * @return array reformed version of $options
+ */
+	function _addPlaceholder($fieldName, $options) {
+		if (isset($options['placeholder'])) {
+			if ($options['placeholder'] === true) {
+				if (!empty($options['label'])) {
+					$options['placeholder'] = $options['label'];
+				} else {
+					$options['placeholder'] = $this->_labelText($fieldName);
+				}
+			}
+		}
+		return $options;
 	}
 }
