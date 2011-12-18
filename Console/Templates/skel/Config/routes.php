68ea5791ace0d9c7c8cd5e4e058f3b40fb3a34ce
unknown <sam.sherlock@gmail.com>
2011-12-18 03:44:02 +0000

    update route docblock and add admin false to login short route

--- Console/Templates/skel/Config/routes.php
+++ Console/Templates/skel/Config/routes.php
@@ -1,31 +1,49 @@
 <?php
 /**
- * Routes Configuration
+ * Routes configuration
  *
  * In this file, you set up routes to your controllers and their actions.
  * Routes are very important mechanism that allows you to freely connect
  * different urls to chosen controllers and their actions (functions).
  *
- * PHP versions 4 and 5
+ * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.app.config
+ * @package       app.Config
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+
+/**
+ * Parse common extensions...
+ */
 Router::parseExtensions('json', 'xml', 'rss', 'ajax');
  
+/**
+ * Here, we are connecting '/' (base path) to controller called 'Pages',
+ * its action called 'display', and we pass a param to select the view file
+ * to use (in this case, /app/View/Pages/home.ctp)...
+ */
 Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
-Router::connect('/logout', array('controller' => 'users', 'action' => 'logout'));
+/**
+ * ...and connect the rest of 'Pages' controller's urls.
+ */
+	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
+/**
+ * Here, we are connecting '/' (base path) to controller called 'Pages',
+ * its action called 'display', and we pass a param to select the view file
+ * to use (in this case, /app/View/Pages/home.ctp)...
+ */
+Router::connect('/login', array('admin' => false, 'controller' => 'users', 'action' => 'login'));
+Router::connect('/logout', array('admin' => false, 'controller' => 'users', 'action' => 'logout'));
 
 /*
  * Localization
